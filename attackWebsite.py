4a2cabef9beada660622e964adbe32f7e501e83b
Andy Wang <z5161179@student.unsw.edu.au>
2020-03-29 23:30:44 +1100

    ready for testing

--- attackWebsite.py
+++ attackWebsite.py
@@ -35,4 +35,4 @@ if __name__ == '__main__':
     dictionary.addWords(wordList)
 
     # perform the attack! You can specify which attacks to use if you want
-    password = attacker.runAttack(checkSuccess, findAll=True, testMode=False, verbose=False, log=True)
+    attacker.runAttack(checkSuccess, findAll=True, testMode=False, verbose=False, log=True)
