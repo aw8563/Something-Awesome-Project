82531a64c4f6df7ff6a0556716d252471d481e03
Andy Wang <z5161179@student.unsw.edu.au>
2020-03-30 09:43:49 +1100

    added host and port configs

--- LoginSystems/__init__.py
+++ LoginSystems/__init__.py
@@ -1,17 +1,20 @@
 from LoginSystems import BadLoginSystem, AverageLoginSystem, GoodLoginSystem
 
 class LoginSystemManager():
-    def __init__(self):
+    def __init__(self, host='localhost', port=5000):
         self.bad = BadLoginSystem.app
         self.average = AverageLoginSystem.app
         self.good = GoodLoginSystem.app
 
+        self.host = host
+        self.port = port
+
     def runBad(self):
-        self.bad.run()
+        self.bad.run(self.host, self.port)
 
     def runAverage(self):
-        self.average.run()
+        self.average.run(self.host, self.port)
 
     def runGood(self):
-        self.good.run()
+        self.good.run(self.host, self.port)
 
